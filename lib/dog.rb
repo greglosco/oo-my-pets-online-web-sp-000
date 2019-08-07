45898e173e3af0cc96639f0f89434159f05a8a2f
Gregory Losco <gtlosco@gmail.com>
2019-08-07 16:09:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,9 @@ class Dog
   attr_accessor :owner, :mood
   attr_reader :name
   
-  def initialize(name, owner, mood)
+  def initialize(name, owner)
     @name = name
     @owner = owner
-    @mood = mood
   end
   
 end
